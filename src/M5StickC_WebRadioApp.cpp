6f23bb1c7ed213d8fcb65dbdb1632e2a817396ad
Ernst Sikora <ernst@sikora-online.de>
2022-02-28 16:08:49 +0100

    Copyright and license information

--- src/M5StickC_WebRadioApp.cpp
+++ src/M5StickC_WebRadioApp.cpp
@@ -1,3 +1,24 @@
+/**
+    M5StickC_WebRadioApp:
+    This application has been developed to use an M5StickC Plus device
+    (ESP32) as internet radio or bluetooth (A2DP) audio receiver.
+    
+    Copyright (C) 2022 by Ernst Sikora
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <M5StickCPlus.h>
 #include <driver/dac.h>
 #include "Audio.h"
